9fa0adc3321d0e8902ec6442ad4ae52b10d9f13f
Ext-ON <rychyaks@icloud.com>
2024-12-18 10:29:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1st program
+print("Первая задача")
+print(9 ** 0.5 * 5)
+# 2nd program
+print("Вторая задача")
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print("Третья задача")
+print("2*2+2") , print("2*(2+2)")
+print(2*2+2 == 2*(2+2))
+# 4th program
+print("Четвёртая задача")
+a = 123.456
+print(a)
+print(int(float(a) * 10) % 10)
